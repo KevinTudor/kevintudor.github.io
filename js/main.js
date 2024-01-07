06a038eea268ff3ea8d1617bd6ae7ff5829052e5
KevinTudor <KevinTudor.work@gmail.com>
2024-01-06 20:54:54 -0500

    stickyHeader

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 // MAIN JS
 
-window.loadTemplate = function(placeHolder, templatePath) {
+var specificPagePath = ''; // Global variable to store the specific page path
+
+window.loadTemplate = function(placeHolder, templatePath, contentPath) {
+    specificPagePath = contentPath; // Store the page path
+
     $(placeHolder).load(templatePath, function(response, status, xhr) {
         if(status == "error") {
-            console.log("Error loading \(" + templatePath + "\) : " + xhr.status + " " + xhr.statusText);
+            console.log("Error loading " + templatePath + " : " + xhr.status + " " + xhr.statusText);
         } else {
-            console.log("\(" + templatePath + "\) loaded successfully.");
+            console.log(templatePath + " loaded successfully.");
+            if(specificPagePath) {
+                window.loadFile('#pageContent', specificPagePath); // Load the specific page
+            }
         }
     });
 }
