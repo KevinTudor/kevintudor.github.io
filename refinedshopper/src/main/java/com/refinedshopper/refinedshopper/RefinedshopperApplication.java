ee58a7a39749b0e7be7949c162d88122d35cd286
KevinTudor <KevinTudor.work@gmail.com>
2024-02-25 23:46:13 -0500

    Clean controller

--- refinedshopper/src/main/java/com/refinedshopper/refinedshopper/RefinedshopperApplication.java
+++ refinedshopper/src/main/java/com/refinedshopper/refinedshopper/RefinedshopperApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Controller
@@ -23,11 +21,6 @@ public class RefinedshopperApplication {
     public String World(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "home";
-    }
-
-    @RequestMapping(value = "/greet", method=RequestMethod.GET)
-    public ModelAndView greet(@RequestParam(value = "name", required=false, defaultValue="World!")final String name, final Model model) {
-        return new ModelAndView("home", "name", name);
     }
  
 }
