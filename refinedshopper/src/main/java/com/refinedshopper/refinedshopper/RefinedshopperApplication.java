10d10649cd8ee707adefdc227f3d1dcd8ec21d5c
KevinTudor <KevinTudor.work@gmail.com>
2024-02-25 23:51:20 -0500

    Controller split

--- refinedshopper/src/main/java/com/refinedshopper/refinedshopper/RefinedshopperApplication.java
+++ refinedshopper/src/main/java/com/refinedshopper/refinedshopper/RefinedshopperApplication.java
@@ -3,24 +3,12 @@ package com.refinedshopper.refinedshopper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@Controller
 public class RefinedshopperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RefinedshopperApplication.class, args);
     }
 
-    @GetMapping("/World")
-    public String World(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "home";
-    }
- 
 }
---
+++ refinedshopper/src/main/java/com/refinedshopper/refinedshopper/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.refinedshopper.refinedshopper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/World")
+    public String World(@RequestParam(value = "name", defaultValue = "Worlds") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+}
